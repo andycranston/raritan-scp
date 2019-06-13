527b968701fdae02b89c99f0f17568446fad74d4
andycranston <acranston@gmail.com>
2019-06-13 13:56:38 +0100

    First production ready version

--- compareconfigs.py
+++ compareconfigs.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python3
 #
-# @(!--#) @(#) compareconfigs.py, version 006, 07-june-2019
+# @(!--#) @(#) compareconfigs.py, version 012, 12-june-2019
 #
 # compare two raw_config.txt files extracted from a Raritan PDU
 #
@@ -20,9 +20,113 @@ import cgitb; cgitb.enable()  # for troubleshooting
 
 #################################################################
 
-DEFAULT_FIRST_FILENAME  = 'raw_config.txt.001'
-DEFAULT_SECOND_FILENAME = 'raw_config.txt.002'
-DEFAULT_FORMAT = 'text'
+def errorpage(errormessage):
+    print('Content-type: text/html')
+    print('')
+    print('</html>')
+    print('<head>');
+    print('<title>Unexpected error</title>')
+    print('</head>');
+    print('<body>')
+
+    print('<h1>An unexpected error has occurred</h1>')
+
+    print('<p>')
+    print('This web page has encountered an error it cannot (currently) deal with.')
+    print('</p>')
+
+    print('<p>')
+    print('This is the error message:')
+    print('</p>')
+
+    print('<pre>')
+    print(html.escape(errormessage))
+    print('</pre>')
+
+    print('<p>')
+    print('Try again as this may be a temporary error.')
+    print('</p>')
+
+    print('<p>')
+    print('If the error keeps happening please contact:')
+    print('</p>')
+
+    print('<pre>')
+    print('andy@cranstonhub.com')
+    print('</pre>')
+
+    print('<hr>')
+
+    print('</body>')
+    print('</html>')
+
+    return
+
+#################################################################
+
+def unixbasename(filename, extension):
+    lenfilename = len(filename)
+
+    lenext = len(extension)
+
+    if lenext < lenfilename:
+        if filename[-lenext:] == extension:
+            filename = filename[0:lenfilename-lenext]
+
+    return filename
+
+#################################################################
+
+def printenv(envname):
+    try:
+        envvalue = os.environ[envname]
+    except KeyError:
+        envvalue = '<undefined>'
+
+    if envvalue == '':
+        envvalue = '<null>'
+
+    print('Environment variable {}="{}"'.format(envname, envvalue))
+
+    return
+
+#################################################################
+
+def safefilenamechars(filename):
+    filename = filename.lower()
+
+    safefilename = ''
+
+    for c in filename:
+        if c in '01234567890abcdefghijklmnopqrstuvwxyz':
+            safefilename += c
+        elif c in '._-':
+            safefilename += c
+        else:
+            safefilename += '_'
+
+    return safefilename
+
+#################################################################
+
+def uploadfilename(stem):
+    try:
+        docroot = os.environ['DOCUMENT_ROOT']
+    except KeyError:
+        return None
+
+    try:
+        remoteaddr = os.environ['REMOTE_ADDR']
+    except KeyError:
+        return None
+
+    remoteaddr = safefilenamechars(remoteaddr)
+
+    pid = os.getpid()
+
+    fname = '{}/uploads/rawconfig_{}_{:05d}_{}'.format(docroot, remoteaddr, pid, stem)
+
+    return fname
 
 #################################################################
 
@@ -69,121 +173,71 @@ def readrawconfig(configfilename):
         
 #################################################################
 
-def convertrawconfigbytes(bytestream):
-    config = {}
+def compareconfigs(first, second):
+    diffcount = 0
 
-    for line in bytestream.split('\n'):
-        line = line.strip()
-        if len(line) == 0:
-            continue
-        if line[0] == '#':
-            continue
-
-        equalsposition = line.find('=')
-    
-        if equalsposition == -1:
-            continue
-    
-        key = line[0:equalsposition]
-        value = line[equalsposition+1:]
-    
-        print('+++ {} +++ {} +++'.format(key, value))
-    
-        if key in config:
-            pass
-        else:
-            config[key] = value
-
-    return config
-
-#################################################################
-
-def compareconfigs(first, second, format):
-    diffs = {}
-
-    index = 0
+    # report keys in first config that are not on the second
+    for key in first:
+        if key not in second:
+            diffcount += 1
+            print('<div class="key">Key "{}" deleted</div>'.format(key))
+            print('<div class="deleted">Old value = "{}"</div>'.format(first[key]))
 
     # report keys in second config that are not in the first
     for key in second:
         if key not in first:
-            if format == 'text':
-                diffs[index] = 'Key "{}" added\n  New value = "{}"'.format(key, second[key])
-            index += 1
+            diffcount += 1
+            print('<div class="key">Key "{}" added</div>'.format(key))
+            print('<div class="added">New value = "{}"</div>'.format(second[key]))
 
     # report keys in both that have a different value
     for key in first:
         if key in second:
             if first[key] != second[key]:
-                if format == 'text':
-                    diffs[index] = 'Key "{}" changed\n  Old value = "{}"\n  New value = "{}"'.format(key, first[key], second[key])
-                index += 1
+                diffcount += 1
+                print('<div class="key">Key "{}" changed</div>'.format(key))
+                print('<div class="changed">Old value = "{}"</div>'.format(first[key]))
+                print('<div class="changed">New value = "{}"</div>'.format(second[key]))
 
-    # report keys in first config that are not on the second
-    for key in first:
-        if key not in second:
-            if format == 'text':
-                diffs[index] = 'Key "{}" deleted\n  Old value = "{}"'.format(key, first[key])
-            index += 1
-
-    return diffs
-
-#################################################################
-
-def commandline():
-    global progame
-
-    parser = argparse.ArgumentParser()
-        
-    parser.add_argument('first',  help='name of first raw_config.txt file')
-    parser.add_argument('second', help='name of second raw_config.txt file')
-
-    args = parser.parse_args()
-
-    firstfilename  = args.first
-    secondfilename = args.second
-
-    firstconfig = readrawconfig(firstfilename)
-
-    if firstconfig == None:
-        sys.exit(2)
-
-    secondconfig = readrawconfig(secondfilename)
-
-    if secondconfig == None:
-        sys.exit(2)
-
-    diffs = compareconfigs(firstconfig, secondconfig, 'text')
-
-    numdiffs = len(diffs)
-
-    if numdiffs == 0:
-        retcode = 0
+    if diffcount == 0:
+        print('<div class="nodifference">No differences found</div>')
     else:
-        for i in range(0, numdiffs):
-            print(diffs[i])
-        retcode = 1
+        print('<br>')
+        print('<div class="endcomparison">End of comparison report</div>')
 
-    return retcode
-
-#################################################################
-
-def formbytes2file(cgiform, fieldname, outfilename):
-    fileitem = cgiform[fieldname]
-    allbytes = fileitem.file.read()
-
-    filehandle = open(outfilename, 'wb')
-    filehandle.write(allbytes)
-    filehandle.flush()
-    filehandle.close()
-
-    return
+    return 
 
 #################################################################
 
-def htmlform():
+def main():
     title = 'Raritan raw configuration online comparison utility'
 
     scriptname = os.path.basename(sys.argv[0])
+
+    cssname = unixbasename(scriptname, '.py') + '.css'
+
+    form = cgi.FieldStorage()
+   
+    compare = form.getfirst('compare', '')
+
+    if compare != '':
+        configs = {}
+
+        for uploadname in [ 'firstconfig', 'secondconfig' ]:
+            filename = uploadfilename(uploadname + '.txt')
+            if filename == None:
+                errorpage('Cannot construct an upload file name for the {} file'.format(uploadname))
+                sys.exit(1)
+
+            fileitem = form[uploadname]
+            allbytes = fileitem.file.read()
+        
+            filehandle = open(filename, 'wb')
+            filehandle.write(allbytes)
+            filehandle.flush()
+            filehandle.close()
+
+            configs[uploadname] = readrawconfig(filename)
 
     print('Content-type: text/html')
     print('')
@@ -194,56 +248,39 @@ def htmlform():
 
     print('<title>{}</title>'.format(html.escape(title)))
 
+    print('<link rel="stylesheet" type="text/css" href="{}">'.format(cssname))
+
     print('</head>');
 
     print('<body>')
 
     print('<h1>{}</h1>'.format(html.escape(title)))
 
-    form = cgi.FieldStorage()
+    ### print('<pre>')
+    ### print(scriptname)
+    ### print(cssname)
+    ### print('</pre>')
 
-   
-    compare = form.getfirst('compare', '')
+    print('<form class="main" enctype="multipart/form-data" method="post" action="{}">'.format(scriptname))
 
-    print('<form enctype="multipart/form-data" method="post" action="{}">'.format(scriptname))
-
-    print('<input type="file" name="firstconfig" size="80" value="Select first raw configuration file">')
-
-    print('<br>')
-    print('<br>')
-
-    print('<input type="file" name="secondconfig" size="80" value="Select second raw configuration file">')
+    print('<div class="prompttext">Select the first raw config file</div>')
+    print('<input class="file" type="file" name="firstconfig">')
 
     print('<br>')
     print('<br>')
 
-    print('<input type="submit" name="compare" value="Compare Configurations">')
+    print('<div class="prompttext">Select the second raw config file</div>')
+    print('<input class="file" type="file" name="secondconfig">')
+
+    print('<br>')
+    print('<br>')
+
+    print('<input class="submit" type="submit" name="compare" value="Compare Configurations">')
 
     print('</form>')
 
     if compare != '':
-        print('<pre>')
-
-        print('Something happening here')
-
-        formbytes2file(form, 'firstconfig', '/tmp/firstconfig.txt')
-        formbytes2file(form, 'secondconfig', '/tmp/secondconfig.txt')
-
-        firstconfig = readrawconfig('/tmp/firstconfig.txt')
-        secondconfig = readrawconfig('/tmp/secondconfig.txt')
-
-        diffs = compareconfigs(firstconfig, secondconfig, 'text')
-
-        numdiffs = len(diffs)
-
-        if numdiffs == 0:
-            retcode = 0
-        else:
-            for i in range(0, numdiffs):
-                print(diffs[i])
-            retcode = 1
-
-        print('</pre>')
+        compareconfigs(configs['firstconfig'], configs['secondconfig'])
 
     print('</body>')
 
@@ -252,23 +289,6 @@ def htmlform():
     return 0
 
 #################################################################
-
-def main():
-    try:
-        gatewayinterface = os.environ['GATEWAY_INTERFACE']
-    except KeyError:
-        gatewayinterface = ''
-
-    if gatewayinterface == '':
-        retcode = commandline()
-    else:
-        retcode = htmlform()
-
-    return retcode
-
-#################################################################
-
-progname = os.path.basename(sys.argv[0])
 
 sys.exit(main())
 
